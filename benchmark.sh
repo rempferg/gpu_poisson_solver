0bc6767c3a06277103d9094a3f1cc75c1c007ac4
Georg Rempfer <github@wo-geht-was.net>
2013-01-12 20:04:19 +0100

    Set upper boundary of linear grid size in benchmarking script to 410.

--- benchmark.sh
+++ benchmark.sh
@@ -2,7 +2,7 @@
 
 echo '#number of nodes along the side of the cubic domain; total number of nodes; calculation time in ms' > runtime_n_time.dat
 
-for i in {1..10}; do
+for i in {1..410}; do
   runtime="`./a.out $i $i $i 1 2> /dev/null | grep Net | cut -d ':' -f 2 | cut -d ' ' -f 2`"
   
   if [ "$runtime" != "" ]; then
